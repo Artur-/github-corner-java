4ef5800046623af789d5cb7cdb6cb30dac34251e
Artur Signell <artur@vaadin.com>
2019-05-10 21:36:48 +0300

    Make it actually work

--- src/main/java/org/vaadin/artur/github_corner/GitHubCorner.java
+++ src/main/java/org/vaadin/artur/github_corner/GitHubCorner.java
@@ -4,11 +4,13 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.PropertyDescriptor;
 import com.vaadin.flow.component.PropertyDescriptors;
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.html.Anchor;
 
 @Tag("github-corner")
 @NpmPackage(value = "github-corner", version = "2.0.3")
+@JsModule("github-corner/index.js")
 public class GitHubCorner extends Component {
 
     private Anchor anchor;
@@ -19,6 +21,7 @@ public class GitHubCorner extends Component {
 
     public GitHubCorner(String githubUrl) {
         this.anchor = new Anchor();
+        getElement().appendChild(this.anchor.getElement());
         setHref(githubUrl);
     }
 
